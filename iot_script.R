340f468b37b72120f7361f94b59ffd54f36ebcf3
KEFunk <funk.katharina10@web.de>
2018-08-31 17:46:46 +0200

    Changed getCSVData

--- iot_script.R
+++ iot_script.R
@@ -10,9 +10,9 @@ library(lubridate)
 ### Import CSV Data ###
 # TODO: change location
 
-getCSVData <-  function(channel) {
+getCSVData <-  function(channel,api_key) {
   # Get CSV File
-  ECOTRON <- read_csv(url(paste("http://thingspeak.umwelt-campus.de/channels/", channel, "/feed.csv", sep="")))
+  ECOTRON <- read_csv(url(paste("http://thingspeak.umwelt-campus.de/channels/", channel, "/feeds?api_key=", api_key, sep="")))
   
   ### Name columns #############################################
   # TODO: Change dummies: a,b,c... to variables
@@ -22,37 +22,40 @@ getCSVData <-  function(channel) {
   ECOTRON$Date <- as.POSIXct(ECOTRON$Date, "%Y-%m-%d %H:%M:%S", tz="UTC")
   # Change timezone
   ECOTRON$Date <- with_tz(ECOTRON$Date, "CET")
+  
+  ### Slice data, define start date and end date
+  # start: 31.8.2018, 6pm
+  # end: 1.9.2018, 9am
+  # TODO: Change actual start and end date
+  
+  
   ECOTRONsub<- subset(ECOTRON, ECOTRON$Date >='2018-08-31 13:15:02' &
                         ECOTRON$Date <= '2018-08-31 14:00:36')
 
   return(ECOTRON)
 }
 
-plotEcotronData <- function(ECOTRON1,ECOTRON2, ECOTRON3) {
-  ### Slice data, define start date and end date
-  # start: 31.8.2018, 6pm
-  # end: 1.9.2018, 9am
-  # TODO: Change actual start and end date
+plotEcotronData <- function(ECOTRON1, ECOTRON2, ECOTRON3, variable) {
+
+  
   
   # Temperatur
-  plot(ECOTRON1$b ~ ECOTRON1$Date, type="l", 
+  plot(ECOTRON1[[variable]] ~ ECOTRON1$Date, type="l", 
        col="green",format="%Y-%m-%d %H:%M:%S")
-  lines(ECOTRON2$b~ECOTRON2$Date, col="red")
-  lines(ECOTRON3$b~ECOTRON3$Date, col="blue")
-  legend("topright", title="CO2 Measurement", c("ECO1", "ECO2", "ECO3"), fill=c("green","red","blue"), 
+  lines(ECOTRON2[[variable]]~ECOTRON2$Date, col="red")
+  lines(ECOTRON3[[variable]]~ECOTRON3$Date, col="blue")
+  legend("topright", title=paste(variable, " Measurement", sep=""), c("ECO1", "ECO2", "ECO3"), fill=c("green","red","blue"), 
          bg = "gray90", cex=0.8, inset=.05)
 }
 
-ECOTRON1 <- getCSVData(318)
-ECOTRON2 <- getCSVData(319)
-ECOTRON3 <- getCSVData(320)
+ECOTRON1 <- getCSVData(318, "KYYV1D41TADHWA6X")
+ECOTRON2 <- getCSVData(319, "BJT4PQDOKAWQVGAD")
+ECOTRON3 <- getCSVData(320, "G03E8ZGJNU37MVMZ")
 
 
-plotEcotronData(ECOTRON1, ECOTRON2, ECOTRON3)
+plotEcotronData(ECOTRON1, ECOTRON2, ECOTRON3, "b")
 
 
-processEcotronData(ECOTRON2)
 
 
-processEcotronData(ECOTRON3)
 
